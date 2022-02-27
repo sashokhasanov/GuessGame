8c4f70945f41b2dad1c09ba895a7f425e3d9a7ee
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-02-27 18:32:51 +0300

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Сашок on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
